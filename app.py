72dcdf3b327b2a03cac69cda4344fc3d45b58f2a
Jacob Shiohira <jlshiohira23@gmail.com>
2020-01-13 02:40:27 -0600

    Fix syntax error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from urllib.request import Request, urlopen
 
 from flask import Flask, request
 
-from mortarboard_dates_bot.src.groupings
+# from mortarboard_dates_bot.src.groupings
 
 app = Flask(__name__)
 
@@ -37,12 +37,17 @@ def should_generate_response(data):
     return True
 
 
+CURRENT_DATES_COMMAND = "current dates"
+NEW_DATES_COMMAND = "new dates"
+HELP_COMMAND = "help"
 def generate_response(data):
     msg = " ".join(data['text'].split(" ")[1:])
-    if msg == CURRENT_DATES:
-        return "current dates":
-    elif msg == NEW_DATES:
-        return "new dates":
+    if msg == CURRENT_DATES_COMMAND:
+        return "current dates"
+    elif msg == NEW_DATES_COMMAND:
+        return "new dates"
+    elif msg == HELP_COMMAND:
+        return "possible commands"
     return None
 
 
