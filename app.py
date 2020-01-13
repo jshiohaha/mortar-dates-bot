f991e3637336e2ae3b13886649611bcfa46c0266
Jacob Shiohira <jlshiohira23@gmail.com>
2020-01-12 21:48:02 -0600

    Change bot to only reply to itself

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+BOT_NAME = 'mbdates'
 
 def send_message(msg):
     url = 'https://api.groupme.com/v3/bots/post'
@@ -21,13 +22,21 @@ def send_message(msg):
     json = urlopen(request).read().decode()
 
 
+def should_respond(msg):
+    # check if message references bot name
+    if msg.split(" ")[0] != BOT_NAME:
+        return False
+    return True
+
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
+    message = data['text']
 
-    # ignore our own messages
-    if data['name'] != 'mortarboard-dates-bot':
-        msg = '{}, you sent "{}"'.format(data['name'], data['text'])
-        send_message(msg)
+    if should_respond(message):
+        # ignore our own messages
+        if data['name'] != BOT_NAME:
+            msg = '{}, you sent "{}"'.format(data['name'], message)
+            send_message(msg)
 
     return "OK", 200
